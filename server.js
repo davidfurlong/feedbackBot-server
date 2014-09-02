1e526fde12aee4ec15502906b3d10fd190c95a97
David Furlong <dvfurlong@googlemail.com>
2014-09-01 22:23:55 -0700

    attempt to fix

--- server.js
+++ server.js
@@ -97,6 +97,6 @@ function response(){
 
 //Start server
 var port = 3000;
-app.listen(port, function() {
-    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+app.listen( process.env.PORT || port, function() {
+    console.log( 'Express server listening on port %d in %s mode', this.address().port, app.settings.env );
 });
