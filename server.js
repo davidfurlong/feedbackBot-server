fa5cd880302c9a1468ddc2e706db26867a90b933
David Furlong <dvfurlong@googlemail.com>
2014-09-02 10:52:53 -0700

    CORS

--- server.js
+++ server.js
@@ -22,10 +22,20 @@ github.authenticate({
     token: "c403e9ea6ffc60730e761a95d67194f5eb4a271a"
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(allowCrossDomain);
     app.use(app.router);
+
 
     // if(DEBUG) {
     //   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
