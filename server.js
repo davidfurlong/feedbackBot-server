c8de6db3b4415e552307561de13f88de90424f85
David Furlong <dvfurlong@googlemail.com>
2014-09-02 11:39:00 -0700

    testing guide

--- server.js
+++ server.js
@@ -1,113 +1,123 @@
-var application_root = __dirname,
-    express = require('express');
-    DEBUG = true;
-
-var app = express();
-
-var GitHubApi = require("github");
-
-var github = new GitHubApi({
-    // required
-    version: "3.0.0",
-    // optional
-    // debug: true,
-    protocol: "https",
-    // host: "",
-    pathPrefix: "/api/v3", // for some GHEs
-    timeout: 5000
-});
-
-github.authenticate({
-    type: "oauth",
-    token: "c403e9ea6ffc60730e761a95d67194f5eb4a271a"
-});
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
-
-
-    // if(DEBUG) {
-    //   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    // }
-});
-
-// app.get('/api/books', function(request, response) {
-//     var books = [
-//             {
-//                 title: "Book 1",
-//                 author: "Author 1",
-//                 releaseDate: "01/01/2014"
-//             },
-//             {
-//                 title: "Book 2",
-//                 author: "Author 2",
-//                 releaseDate: "02/02/2014"
-//             }
-//         ];
-
-//     response.send(books);
-// });
-//Insert a new book
-app.get('/', function( request, response ) {
-    var feedback_item = {
-        url: request.body.url,
-        title: request.body.title,
-        labels: request.body.labels,
-        body: request.body.body
-    };
-    if(feedback_item.url != undefined){
-        toGithub(feedback_item);
-        response.send('Data passed');
+// Include our HTTP module.
+var http = require( "http" );
+ 
+ 
+// Create an HTTP server so that we can listen for, and respond to
+// incoming HTTP requests. This requires a callback that can be used
+// to handle each incoming request.
+var server = http.createServer(
+    function( request, response ){
+ 
+ 
+        // When dealing with CORS (Cross-Origin Resource Sharing)
+        // requests, the client should pass-through its origin (the
+        // requesting domain). We should either echo that or use *
+        // if the origin was not passed.
+        var origin = (request.headers.origin || "*");
+ 
+ 
+        // Check to see if this is a security check by the browser to
+        // test the availability of the API for the client. If the
+        // method is OPTIONS, the browser is check to see to see what
+        // HTTP methods (and properties) have been granted to the
+        // client.
+        if (request.method.toUpperCase() === "OPTIONS"){
+ 
+ 
+            // Echo back the Origin (calling domain) so that the
+            // client is granted access to make subsequent requests
+            // to the API.
+            response.writeHead(
+                "204",
+                "No Content",
+                {
+                    "access-control-allow-origin": origin,
+                    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "access-control-allow-headers": "content-type, accept",
+                    "access-control-max-age": 10, // Seconds.
+                    "content-length": 0
+                }
+            );
+ 
+            // End the response - we're not sending back any content.
+            return( response.end() );
+ 
+ 
+        }
+ 
+ 
+        // -------------------------------------------------- //
+        // -------------------------------------------------- //
+ 
+ 
+        // If we've gotten this far then the incoming request is for
+        // our API. For this demo, we'll simply be grabbing the
+        // request body and echoing it back to the client.
+ 
+ 
+        // Create a variable to hold our incoming body. It may be
+        // sent in chunks, so we'll need to build it up and then
+        // use it once the request has been closed.
+        var requestBodyBuffer = [];
+ 
+        // Now, bind do the data chunks of the request. Since we are
+        // in an event-loop (JavaScript), we can be confident that
+        // none of these events have fired yet (??I think??).
+        request.on(
+            "data",
+            function( chunk ){
+ 
+                // Build up our buffer. This chunk of data has
+                // already been decoded and turned into a string.
+                requestBodyBuffer.push( chunk );
+ 
+            }
+        );
+ 
+ 
+        // Once all of the request data has been posted to the
+        // server, the request triggers an End event. At this point,
+        // we'll know that our body buffer is full.
+        request.on(
+            "end",
+            function(){
+ 
+                // Flatten our body buffer to get the request content.
+                var requestBody = requestBodyBuffer.join( "" );
+ 
+                // Create a response body to echo back the incoming
+                // request.
+                var responseBody = (
+                    "Thank You For The Cross-Domain AJAX Request:\n\n" +
+                    "Method: " + request.method + "\n\n" +
+                    requestBody
+                );
+ 
+                // Send the headers back. Notice that even though we
+                // had our OPTIONS request at the top, we still need
+                // echo back the ORIGIN in order for the request to
+                // be processed on the client.
+                response.writeHead(
+                    "200",
+                    "OK",
+                    {
+                        "access-control-allow-origin": origin,
+                        "content-type": "text/plain",
+                        "content-length": responseBody.length
+                    }
+                );
+ 
+                // Close out the response.
+                return( response.end( responseBody ) );
+ 
+            }
+        );
+ 
+ 
     }
-    response.send('No data being passed');
-});
-
-function toGithub(feedback_item){
-    console.log(feedback_item);   
-    var url = feedback_item.url;
-    var pos = url.indexOf('/');
-    var username = url.substring(0, pos);
-    var repo = url.substring(pos+1);
-
-    github.issues.create({
-            headers: {'User-Agent':'davidfurlong'},
-            user: username,
-            repo: repo,
-            title: feedback_item.title,
-            body: feedback_item.body,
-            labels: feedback_item.labels
-        },
-        response()
-    );
-}
-
-function response(){
-    console.log('done');
-}
-//Get a single book by id
-// app.get( ':id', function( request, response ) {
-//     var book = {
-//         title: "Unique Book",
-//         author: "Unique Author",
-//         releaseDate: "03/03/2014"
-//     };
-    
-//     response.send(book);
-// });
-
-//Start server
-var port = 3000;
-app.listen( process.env.PORT || port, function() {
-    console.log( 'Express server listening on port %d in %s mode', this.address().port, app.settings.env );
-});
+);
+ 
+ 
+// Bind the server to port 8080.
+server.listen( 3000 );
+ 
